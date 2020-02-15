d513403b235d1af165b50ae42dadaa46d7ba1a7a
Xichao Wang <xichaowxc@gmail.com>
2020-02-16 09:49:49 +1100

    successfully connect to aws rds mysql.

---
+++ connection.R
@@ -0,0 +1,16 @@
+library(DBI)
+library(RMySQL)
+
+db_user <- 'admin'
+db_password <- ''
+db_name <- 'testdb'
+# this is AWS endpoint address
+db_host <- ''
+db_port <- 3306
+
+mydb <- dbConnect(MySQL(), 
+                  user = db_user, 
+                  password = db_password, 
+                  dbname = db_name, 
+                  host = db_host, 
+                  port = db_port)
